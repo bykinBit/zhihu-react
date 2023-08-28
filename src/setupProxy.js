527aeec9901fcb24e5e7d6acda4b436b1585bd82
byking <sunbyking@gmail.com>
2023-08-28 14:50:52 +0800

    修改配置

---
+++ src/setupProxy.js
@@ -0,0 +1,20 @@
+/***/
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "http://localhost:3000",
+      changeOrigin: true,
+      pathRewrite: { "^/api": "" },
+    })
+  );
+  app.use(
+    createProxyMiddleware("/websocket", {
+      target: "http://localhost:3000",
+      ws: true,
+      changeOrigin: true,
+    })
+  );
+};
